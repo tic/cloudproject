058f7c67f61a80ac2fea207a498d1d45dc671903
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2021-03-09 15:48:39 -0500

    create some skeletons

---
+++ rtsatd.py
@@ -0,0 +1,13 @@
+
+# Algorithm 1: RTSATD
+def rtsatd_main():
+    # while new workflows arrive...
+    pass
+#
+
+
+# Algorithm 2: Function TaskSchedule()
+def task_schedule():
+    # setSI <-- NULL; ...
+    pass
+#
