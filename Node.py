6c277f5663be2625752f5a5e39124ada4fca5cf4
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2021-04-22 15:48:26 -0400

    stash a few task methods and add node types

--- Node.py
+++ Node.py
@@ -3,17 +3,33 @@ import asyncio
 
 class Node(object):
     id = 0
-    def __init__(self, process_speed=1):
-        self.speed = process_speed
-        self.working = False
+
+    # Machine types define process, read, and write speeds
+    # The overall processing multiplier of the node is factored in later
+    DedicatedMachine = (100, 100, 100)  # Type 0
+    DedicatedCore = (15, 15, 15)        # Type 1
+    DedicatedContainer = (3, 3, 3)      # Type 2
+    SharedContainer = (1, 1, 1)         # Type 3
+
+    def __init__(self, type, process_speed=1):
         self.__id = Node.id
         Node.id += 1
 
+        self.working = False
+
+        self.type = type
+        if type == 0: self.proc_speed, self.read_speed, self.write_speeed = Node.DedicatedMachine
+        elif type == 1: self.proc_speed, self.read_speed, self.write_speeed = Node.DedicatedCore
+        elif type == 2: self.proc_speed, self.read_speed, self.write_speeed = Node.DedicatedContainer
+        else: self.proc_speed, self.read_speed, self.write_speeed = Node.SharedContainer
+
     async def run(self, task):
         # print(f'node {self.__id} working')
-        await task.setup(self.speed)
-        await task.run(self.speed)
-        await task.finish(self.speed)
+        self.task = task
+        await task.setup(self.proc_speed)
+        await task.run(self.proc_speed)
+        await task.finish(self.proc_speed)
+        self.task = None
         self.working = False
         # print(f'node {self.__id} done!')
 
--- Task.py
+++ Task.py
@@ -1,15 +1,25 @@
 
-KBs = lambda x : x * 1024
+KBs = lambda x : x * 1024.0
 MBs = lambda x : x * KBs(1024)
 
 from asyncio import sleep
+from functools import reduce
+import operator
 class Task(object):
     def __init__(self, job_details, dupe=False):
         self.__done = False
         self.__ready = dupe
         self.__job = job_details
-        self.__file_read_speed = MBs(500)
-        self.__file_write_speed = MBs(370)
+        self.__file_read_speed = MBs(110)
+        self.__file_write_speed = MBs(75)
+
+        # no speedup effect computed here. could add this in elsewhere if still wanted
+        self.start_time = reduce(operator.add, map(lambda t: t['size'], filter(lambda file: file['link'] == 'input', job_details['files']))) / self.__file_read_speed
+
+        # need to factor in parent tasks
+        self.completion_time = job_details['runtime']
+
+        self.data_time = reduce(operator.add, map(lambda t: t['size'], filter(lambda file: file['link'] == 'output', job_details['files']))) / self.__file_read_speed
 
     def duplicate(self):
         t = Task(self.__job, dupe=True)
@@ -22,7 +32,7 @@ class Task(object):
         # sleep based on input file read time
         sleep_time = 0
         for file in self.__job['files']:
-            if file['link'] == 'output':
+            if file['link'] == 'input':
                 sleep_time += file['size']
         sleep_time /= self.__file_read_speed
         await sleep(sleep_time / speed)
@@ -42,3 +52,41 @@ class Task(object):
 
         sleep_time /= self.__file_write_speed
         await sleep(sleep_time / speed)
+
+    @staticmethod
+    def computeLCT(task):
+        # Compute LCT as done in the paper
+
+        successor_tasks = [] # get the list of successor tasks, somehow
+        if len(successor_tasks) == 0:
+            return 0
+
+        precursor_tasks = [] # get the list of parent tasks, somehow
+        ct = 0
+        for pt in precursor_tasks:
+            pt_ct = pt.completion_time
+            ct = pt_ct if pt_ct > ct else ct
+
+        lct = ct
+        for pt in precursor_tasks:
+            dt = ct - pt.data_time
+            lct = dt if dt < lct else lct
+
+        return lct
+
+    @staticmethod
+    def computePCT(task):
+        # Compute PCT as done in the paper
+
+        precursor_tasks = []
+
+        if len(precursor_tasks) == 0:
+            pass
+            # return crt + it(t_i^j)
+
+        pct = 0
+        for pt in precursor_tasks:
+            candidate = pt.completion_time # + dt(e^i_(p, j))
+            pct = candidate if candidate < pct else pct
+
+        # pct += mrt(t^i_j)
