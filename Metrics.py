56219539871d6a3a65a323524ea13bccd46e6ee6
Mitchell Campbell <mc5dp@virginia.edu>
2021-05-02 13:01:02 -0400

    updated TC metric calculation

--- Metrics.py
+++ Metrics.py
@@ -15,7 +15,10 @@ def ANCT(task_df, wf_array):
 
 def TC(service_instances):
     # Takes a list of service instances as input
-    return mean([node.provisioned_time * node.cost for node in service_instances])
+    from Task import crt
+    current_time = crt()
+    #node.provisioned time is the clock time for when a node was provisioned
+    return mean([(current_time - node.provisioned_time)* node.cost for node in service_instances])
 
 def RU(service_instances):
     total_working_time = sum([node.execution_time for node in service_instances])
